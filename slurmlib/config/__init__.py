1336014379d09fc79497803a961aa856eb1a6548
Christian Huber <chuber1986@users.noreply.github.com>
2024-04-01 19:20:30 +0200

    feat: load SLURM default configurations from yaml file.

--- slurmlib/config/__init__.py
+++ slurmlib/config/__init__.py
@@ -1,10 +1,41 @@
 """Run configurations package."""
 
 import abc
+import importlib
 from dataclasses import dataclass
 from pathlib import Path
 
+import yaml
+
 from slurmlib import JOB_FILE, WORKERSTUB
+
+
+def _get_target(definition):
+    return definition.get("_target_")
+
+
+def _get_args(definition):
+    return definition.get("_args_", [])
+
+
+def _get_class(definition):
+    splits = _get_target(definition).split(".")
+    package = ".".join(splits[:-2])
+    module = splits[-2]
+    clazz = splits[-1]
+
+    module = importlib.import_module(module, package)
+    return getattr(module, clazz)
+
+
+def _parse_resource(resource):
+    args = _get_args(resource)
+    clazz = _get_class(resource)
+    return clazz(**args)
+
+
+def _parse_resources(resources):
+    return [_parse_resource(res) for res in resources]
 
 
 @dataclass
@@ -16,3 +47,18 @@ class NodeConfig(abc.ABC):
     ) -> None:
         self.job_file = jobfile
         self.workerstub = workerstub
+
+    @staticmethod
+    def load_defaults(config_file: Path):
+        if not config_file.exists() or not config_file.is_file():
+            return {}
+
+        with config_file.open("r") as file:
+            defs = yaml.safe_load(file)
+
+        defaults = {}
+        for name, definition in defs.items():
+            cfg = _parse_resources(definition)
+            defaults[name] = cfg
+
+        return defaults
--- slurmlib/config/slurm.py
+++ slurmlib/config/slurm.py
@@ -3,22 +3,33 @@
 from pathlib import Path
 
 from resources.resources import Resource
-from runner.slurm import SlurmRunner
 
-from slurmlib import JOB_FILE, WORKERSTUB
+from slurmlib import JOB_FILE, SLURMLIB_DIR, WORKERSTUB
 
 from . import NodeConfig
 
 __all__ = ["SlurmConfig"]
 
+_defaults = NodeConfig.load_defaults(SLURMLIB_DIR / "slurm_defaults.yaml")
+
 
 class SlurmConfig(NodeConfig):
+    """Cluster configuration for SLURM"""
+
     def __init__(
         self,
         *resources: Resource,
-        jobfile: Path = JOB_FILE,
-        workerstub: Path = WORKERSTUB,
+        jobfile: Path | str = JOB_FILE,
+        workerstub: Path | str = WORKERSTUB,
     ) -> None:
-        super().__init__(
-            runner=SlurmRunner(*resources, jobfile=jobfile, workerstub=workerstub)
-        )
+        super().__init__(jobfile=jobfile, workerstub=workerstub)
+        self.resources = resources
+
+    @staticmethod
+    def get_default_configurations() -> dict:
+        return dict(_defaults)
+
+
+for key, value in _defaults.items():
+    setattr(SlurmConfig, key, value)
+    globals()[key] = value
