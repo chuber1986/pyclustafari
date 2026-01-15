6f7b93251be2ca83b8f3cd471412e0a3b254cd43
Christian Huber <christian.huber@silicon-austria.com>
2026-01-15 06:50:12 +0100

    fix: paths for dist deployment

--- src/clustafari/paths.py
+++ src/clustafari/paths.py
@@ -1,13 +1,26 @@
 """Important Paths collection."""
 
+import os
 from pathlib import Path
+
+from clustafari import workerstub
 
 CLUSTAFARI_DIR = Path.home() / ".clustafari"
 CLUSTAFARI_DIR.mkdir(exist_ok=True)
 
 ROOT = Path(__file__).parent.parent.parent
-WORKERSTUB = ROOT / "src" / "clustafari" / "workerstub.py"
 JOB_FILE = ROOT / "scripts" / "clustafari.job"
+WORKERSTUB = workerstub.__file__
 
 
-JOB_FILE = JOB_FILE if JOB_FILE.exists() else Path("clustafari.job")
+def _find_path_to(prog: str) -> Path:
+    for p in os.environ["PATH"].split(":"):
+        res = list(Path(p).glob(prog))
+        if res:
+            return res[0]
+
+    msg = "Can't find '%s' in PATH."
+    raise ValueError(msg, prog)
+
+
+JOB_FILE = JOB_FILE if JOB_FILE.exists() else _find_path_to("clustafari.job")
